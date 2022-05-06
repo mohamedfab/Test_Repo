2d285e370f3601620d84c3b915a070289bfce04e
mohamedfab <mo7amed.fawzy33@gmail.com>
2022-05-06 21:07:03 +0200

    Calculator Module Implementation added

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include "calculator.h"
+
+float sum(float num1, float num2)
+{
+	float result = 0;
+	return num1 + num2;
+	
+}
+float sub(float num1, float num2)
+{
+	float result = 0;
+	return num1 - num2;
+	
+}
+float div(float num1, float num2)
+{
+	float result = 0;
+	return num1 / num2;
+	
+}
+float mul(float num1, float num2)
+{
+	float result = 0;
+	return num1 * num2;
+}
---
+++ calculator.h
@@ -0,0 +1,6 @@
+
+
+float sum(float num1, float num2);
+float sub(float num1, float num2);
+float div(float num1, float num2);
+float mul(float num1, float num2);
